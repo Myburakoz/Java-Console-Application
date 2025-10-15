89ab638b7f004fee572e7d006a64886fdef4d8f5
Burak <myburakoz@gmail.com>
2025-10-15 22:35:42 +0300

    The name of .java file changed

--- src/Group06.java
+++ src/Group06.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
-
-public class Main {
+public class Group06 {
     public static void main(String[] args) {
         final String welcome = """
                 ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ████████╗ ██████╗                                             \s
